061218f9fc354a650fb5b3bf1490a854bddc11af
Anish <anish@anaharmacair.local>
2016-09-29 16:48:03 -0400

    Initial commit

--- working.py
+++ working.py
@@ -1,15 +1,4 @@
-# Download the twilio-python library from http://twilio.com/docs/libraries
-from twilio.rest import TwilioRestClient
 
-# Find these values at https://twilio.com/user/account
-account_sid = "AC471b4009becda2f23c3fe90df58dd7cc"
-auth_token = "dae6dc2569f846ab2b9e2404c7b1d876"
-client = TwilioRestClient(account_sid, auth_token)
-
-message = client.messages.create(to="+16264752489", from_="+14159432584",
-                                     body="This is from Pronto's Twilio test app!")
-                                     
-                                     
 from flask import Flask, request, redirect
 import twilio.twiml
 
